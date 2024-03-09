6ad6300ac0a5876f709ef441b76390fa7ec597e8
Joao Castro <110193153+joaogcastro@users.noreply.github.com>
2024-03-08 21:30:10 -0300

    First exercise sample commit

---
+++ js.js
@@ -0,0 +1,8 @@
+console.log("Altair");
+let colors = ["magenta",  "purple", "aqua", "blue", "coral", "gold", "green", "red", "teal", "tomato", "yellow"];
+//Ordena os itens do array por ordem alfabetica
+colors.sort();
+console.log (colors);
+
+document.getElementById("colorDiv").innerHTML = colors;
+colorDiv.innerHTML = colors;
